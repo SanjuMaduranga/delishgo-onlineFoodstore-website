affbeab7260b813eb7836318c0bd375f6cefc5ee
Sanjula Maduranga <104560312+SanjuMaduranga@users.noreply.github.com>
2026-06-17 16:43:43 +0530

    Add files via upload

---
+++ src/app/about/branch/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function branch() {
+  return (
+    <div>
+      <h1>Branch Locater</h1>
+    </div>
+  )
+}
+
+export default branch
---
+++ src/app/about/careers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function careers() {
+  return (
+    <div>
+      <h1>Careers</h1>
+    </div>
+  )
+}
+
+export default careers
